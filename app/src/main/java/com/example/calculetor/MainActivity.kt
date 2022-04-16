43c71b0435b5f617ae39b74bdd7d74e262b16ad9
MSI\harsh <hdp21091995@gmail.com>
2022-04-16 14:56:06 -0400

    Basic logic implementation

--- app/src/main/java/com/example/calculetor/MainActivity.kt
+++ app/src/main/java/com/example/calculetor/MainActivity.kt
@@ -5,59 +5,124 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var displayTv: TextView
-    private lateinit var zeroBtn: Button
-    private lateinit var oneBtn: Button
-    private lateinit var twoBtn: Button
-    private lateinit var threeBtn: Button
-    private lateinit var fourBtn: Button
-    private lateinit var fiveBtn: Button
-    private lateinit var sixBtn: Button
-    private lateinit var sevenBtn: Button
-    private lateinit var eightBtn: Button
-    private lateinit var nineBtn: Button
-    private lateinit var plusBtn: Button
-    private lateinit var minusBtn: Button
-    private lateinit var multiplyBtn: Button
-    private lateinit var divideBtn: Button
-    private lateinit var clearBtn: Button
-    private lateinit var equalsBtn: Button
+    private var lastNumeric: Boolean = true
+    private var lastDot: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        widgetInit()
+        displayTv = findViewById(R.id.tvInput)
     }
 
     fun onDigit(view: View) {
         displayTv.append((view as Button).text)
+        lastNumeric = true
     }
 
     fun onClear(view: View) {
         displayTv.text = ""
     }
 
-    private fun widgetInit() {
-        displayTv = findViewById(R.id.tvInput)
-        zeroBtn = findViewById(R.id.btnZero)
-        oneBtn = findViewById(R.id.btnOne)
-        twoBtn = findViewById(R.id.btnTwo)
-        threeBtn = findViewById(R.id.btnThree)
-        fourBtn = findViewById(R.id.btnFour)
-        fiveBtn = findViewById(R.id.btnFive)
-        sixBtn = findViewById(R.id.btnSix)
-        sevenBtn = findViewById(R.id.btnSeven)
-        eightBtn = findViewById(R.id.btnEight)
-        nineBtn = findViewById(R.id.btnNine)
-        plusBtn = findViewById(R.id.btnPlus)
-        minusBtn = findViewById(R.id.btnMinus)
-        multiplyBtn = findViewById(R.id.btnMultiply)
-        divideBtn = findViewById(R.id.btnDivide)
-        equalsBtn = findViewById(R.id.btnEquals)
-        clearBtn = findViewById(R.id.btnClear)
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !lastDot) {
+            displayTv.append((view as Button).text)
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    fun onOperator(view: View) {
+        displayTv.text.let {
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
+                displayTv.append((view as Button).text)
+                lastDot = false
+                lastNumeric = false
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value: String): Boolean =
+        if (value.startsWith("-"))
+            false
+        else
+            value.contains("+") ||
+                value.contains("-") ||
+                value.contains("*") ||
+                value.contains("/")
+
+    private fun removeZeroAfterDot(result: String): String =
+        if (result.contains(".0")) (result.substring(0, result.length - 2)) else result
+
+    fun onEqual(view: View) {
+        if (lastNumeric) {
+            var tvValue = displayTv.text.toString()
+            var prefix = ""
+            try {
+                if (tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                if (tvValue.contains("/")) {
+                    val splitValue = tvValue.split("/")
+
+                    var one = splitValue[0]
+                    val two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+
+                    displayTv.text =
+                        removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                }
+                if (tvValue.contains("*")) {
+                    val splitValue = tvValue.split("*")
+
+                    var one = splitValue[0]
+                    val two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+
+                    displayTv.text =
+                        removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                }
+                if (tvValue.contains("+")) {
+                    val splitValue = tvValue.split("+")
+
+                    var one = splitValue[0]
+                    val two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+
+                    displayTv.text =
+                        removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }
+                if (tvValue.contains("-")) {
+                    val splitValue = tvValue.split("-")
+
+                    var one = splitValue[0]
+                    val two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+
+                    displayTv.text =
+                        removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                }
+            } catch (e: ArithmeticException) {
+                e.stackTrace
+            }
+        }
     }
 }
